32c5a3ec051c1d45874f841d4a9fcff16ffd8147
Julien Lefebvre <lfj5208a@ensfsi.onmicrosoft.com>
2024-11-18 16:22:44 +0100

    les humains ont des connaissance

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,9 +1,12 @@
 package personnages;
 
 public class Humain {
+	private static final int TAILLE_MEMOIRE=30;
 	private String nom;
 	private String boissonFavorite;
 	protected int argent;
+	protected int nbConnaissance=0;
+	protected Humain[] memoire=new Humain[TAILLE_MEMOIRE];
 
 	public Humain(String nom, String boissonFavorite, int argent) {
 		this.nom = nom;
@@ -38,7 +41,38 @@ public class Humain {
 	protected void gagnerArgent(int argentGagne) {
 		argent += argentGagne;
 	}
+	
+	public void faireConnaissanceAvec(Humain autreHumain) {
+		direBonjour();
+		autreHumain.repondre(this);
+		memoriser(autreHumain);
+	}
+	
+	public void repondre(Humain autreHumain) {
+		direBonjour();
+		memoriser(autreHumain);
+	}
 
+	private void memoriser(Humain autreHumain) {
+		if(nbConnaissance<TAILLE_MEMOIRE) {
+			memoire[nbConnaissance]=autreHumain;
+			nbConnaissance++;
+		}
+		else {
+			for(int i=1;i<TAILLE_MEMOIRE;i++) memoire[i-1]=memoire[i];
+			memoire[TAILLE_MEMOIRE-1]=autreHumain;
+		}
+	}
+	
+	public void listerConnaissance() {
+		String texte="Je connais beaucoup de monde dont : ";
+		if (nbConnaissance>0) {
+			for (int i=0;i<nbConnaissance-1;i++) texte+=memoire[i].getNom()+", ";
+			texte+=memoire[nbConnaissance-1].getNom();
+			parler(texte);
+		}
+	}
+	
 	public void acheter(String bien, int prix) {
 		if (prix <= argent) {
 			parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous");
