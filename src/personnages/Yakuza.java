ee11ec09d913a05e49061d2fc5cfe0f7d1a65889
Julien Lefebvre <lfj5208a@ensfsi.onmicrosoft.com>
2024-11-18 16:42:55 +0100

    les samourais

--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -41,6 +41,6 @@ public class Yakuza extends Humain {
 	@Override
 	public void direBonjour() {
 		super.direBonjour();
-		parler("Mon clan est celui de "+clan);
+		parler("Mon clan est celui de "+clan+".");
 	}
 }
